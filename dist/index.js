1c2350fca72ca0671fa04e8c62e6beb3962ce512
Oleh Iliushyn <oleg.ilyushyn@nbcuni.com>
2023-02-21 14:34:38 +0200

    logs

--- dist/index.js
+++ dist/index.js
@@ -9777,6 +9777,35 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(8481);
 const github = __nccwpck_require__(4633);
 
+const getReleaseDate = (branchName) => {
+    const match = branchName.match(/\d{4}\.\d{2}\.\d{2}/);
+
+    return match ? match[0] : '';
+}
+
+const compareBranches = (branch1, branch2) => {
+    if (branch1 === 'master') {
+        return -1;
+    }
+    if (branch2 === 'master') {
+        return 1;
+    }
+    if (branch1 === 'develop') {
+        return 1;
+    }
+    if (branch2 === 'develop') {
+        return -1;
+    }
+
+    const branch1Date = getReleaseDate(branch1);
+    const branch2Date = getReleaseDate(branch2);
+
+    if (branch2Date === branch1Date) {
+        return 0;
+    }
+
+    return (branch2Date > branch1Date) ? -1 : 1;
+}
 const run = async () => {
     console.log('Starting');
     const currentBranch = core.getInput('current_branch');
@@ -9784,8 +9813,17 @@ const run = async () => {
     console.log('Repo', github.context.repo.repo);
     const githubToken = core.getInput('github_token');
     const octokit = github.getOctokit(githubToken);
-    const allBranches = await octokit.rest.repos.listBranches(github.context.repo);
+    const allBranches = (await octokit.rest.repos.listBranches(github.context.repo)).data
+        .map((branch) => branch.name)
+        .filter(name => name ? name.match(/(^master$|^develop$|^release_|^release\/|^draft_release)/) : false)
+        .sort(compareBranches);
     console.log('allBranches', allBranches);
+    const currentIndex = allBranches.findIndex((name) => name === currentBranch);
+    const targetBranch = allBranches[currentIndex + 1];
+    console.log('targetBranch', targetBranch);
+    if (targetBranch) {
+        core.setOutput('target_branch', targetBranch);
+    }
 };
 
 run().catch((e) => {
