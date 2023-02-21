cbce668b2504327a884871819ee396605fc04f27
Oleh Iliushyn <oleg.ilyushyn@nbcuni.com>
2023-02-21 14:11:38 +0200

    logs

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const github = require('@actions/github');
 const run = async () => {
     const currentBranch = core.getInput('current_branch');
     console.log('Current Branch', currentBranch);
+    console.log('Repo', github.context.repo);
     const githubToken = core.getInput('github_token');
     const octokit = github.getOctokit(githubToken);
     const allBranches = await octokit.rest.repos.listBranches({
@@ -14,6 +15,7 @@ const run = async () => {
 };
 
 run().catch((e) => {
+    console.error(e);
     core.setFailed(e.message);
 }).finally(() => {
     process.exit(0);
